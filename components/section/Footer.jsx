609d4fad3e8c43d11503c1ec67d0afad41b45f12
rickyzatnika <rickyzatnika91@gmail.com>
2023-05-28 20:22:49 +0700

    change footer color

--- components/section/Footer.jsx
+++ components/section/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#6F4E37] text-zinc-50 flex relative w-full h-28 lg:h-auto py-2 flex-nowrap items-start lg:items-center justify-center gap-1">
+    <footer className="bg-[#131313] text-zinc-50 flex relative w-full h-28 lg:h-auto py-2 flex-nowrap items-start lg:items-center justify-center gap-1">
       <p className="text-sm">Powered By : </p>
       <Link href="https://dboard.webqodes.com" className="text-sm">
         WebQodes
